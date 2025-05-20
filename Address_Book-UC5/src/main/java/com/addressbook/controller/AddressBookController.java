7b0d0728b5b0b88a8f28dfbdad267c22ffc80c2e
Pareekshith Paruchuri <pareekshith@Pareekshiths-MacBook.local>
2025-05-20 10:24:28 +0530

    Add UC5 Full Controller

---
+++ Address_Book-UC5/src/main/java/com/addressbook/controller/AddressBookController.java
@@ -0,0 +1,44 @@
+package com.addressbook.controller;
+
+import com.addressbook.dto.AddressBookDTO;
+import com.addressbook.model.AddressBook;
+import com.addressbook.service.IAddressBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+    @Autowired
+    private IAddressBookService service;
+
+    @PostMapping
+    public ResponseEntity<AddressBook> add(@RequestBody AddressBookDTO dto) {
+        return ResponseEntity.ok(service.addAddress(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressBook>> getAll() {
+        return ResponseEntity.ok(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBook> getById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBook> update(@PathVariable int id, @RequestBody AddressBookDTO dto) {
+        return ResponseEntity.ok(service.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        service.delete(id);
+        return ResponseEntity.ok("Deleted ID: " + id);
+    }
+}
